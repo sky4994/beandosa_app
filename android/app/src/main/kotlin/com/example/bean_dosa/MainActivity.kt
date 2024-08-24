2716f2d1f1cdef15debe4241488935233f032f9b
John Hur <johnjeongukhur@gmail.com>
2024-08-24 17:05:52 +0900

    Apply .gitignore and remove Firebase related files

---
+++ android/app/src/main/kotlin/com/example/bean_dosa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bean_dosa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final CounterController controller = Get.put(CounterController());
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            // Obx를 사용하여 상태를 관찰하고 UI를 업데이트합니다.
+            Obx(() => Text(
+              '${controller.counter.value}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            )),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class CounterController extends GetxController {
+  var counter = 0.obs;
+
+  void incrementCounter() {
+    counter.value++;
+  }
+}
